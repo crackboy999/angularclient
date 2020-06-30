e7a80984fb465c69d3b5e3e079882512d51f6170
Itsmartsapp <59887768+Itsmartsapp@users.noreply.github.com>
2020-06-30 14:49:36 -0500

    despliegue

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = express();
 
 app.use(express.static(__dirname+'/dist/my-aplicacion'));
-app.get('/',function(req,res){
+app.get('/*',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/my-aplicacion/index.html'));
 });
 
