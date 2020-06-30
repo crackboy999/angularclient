b8f22fede2ad21f8d0daea18d373e8ec83d1812c
Itsmartsapp <59887768+Itsmartsapp@users.noreply.github.com>
2020-06-30 14:39:55 -0500

    herokdu

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = express();
 
-app.use(express.static(__dirname+'/dist/'));
+app.use(express.static(__dirname+'/dist/my-aplicacion'));
 app.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/dist/my-aplicacion/index.html'));
 });
